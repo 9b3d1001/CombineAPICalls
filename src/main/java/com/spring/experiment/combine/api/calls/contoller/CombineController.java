a413561f180e9f0d388147418dc199378beff1b0
Ishaan Arora <ishaan.arora@nagarro.com>
2024-05-15 14:11:22 +0530

    Adding Comments

--- src/main/java/com/spring/experiment/combine/api/calls/contoller/CombineController.java
+++ src/main/java/com/spring/experiment/combine/api/calls/contoller/CombineController.java
@@ -30,6 +30,7 @@ public class CombineController {
         final CountDownLatch latch = new CountDownLatch(2);
         final String[] responses = new String[2];
         Instant begin = Instant.now();
+        // This approach uses the task exector threads configured via application.properties and execute the API calls on two separate threads
         taskExecutor.execute(() -> {
             responses[0] = countryService.callAPI1(code);
             latch.countDown();
@@ -38,9 +39,10 @@ public class CombineController {
             responses[1] = countryService.callAPI2(code);
             latch.countDown();
         });
+        //Blocking / Waiting for the response of two calls here
         latch.await();
         Instant end = Instant.now();
-        System.out.println("It took " + Duration.between(end, begin).toSeconds());
+        System.out.println("It took " + Duration.between(begin, end).toSeconds());
         return ResponseEntity.ok(  responses[0].substring(0, responses[0].length() - 1) + "," + responses[1].substring(1));
     }
 
@@ -48,12 +50,14 @@ public class CombineController {
     public ResponseEntity<String> getCountryData2(@PathVariable(name = "code") String code) throws InterruptedException, ExecutionException {
         final String[] responses = new String[2];
         Instant begin = Instant.now();
+        // This approach uses the Future Callable Thread Fork-Join pool
         CompletableFuture<String> response0 = countryService.callAPICF1(code);
         CompletableFuture<String> response1 = countryService.callAPICF2(code);
+        //Blocking / Waiting for the response of two calls here
         responses[0] = response0.get();
         responses[1] = response1.get();
         Instant end = Instant.now();
-        System.out.println("It took " + Duration.between(end, begin).toSeconds());
+        System.out.println("It took " + Duration.between(begin, end).toSeconds());
         return ResponseEntity.ok(  responses[0].substring(0, responses[0].length() - 1) + "," + responses[1].substring(1));
     }
 }
